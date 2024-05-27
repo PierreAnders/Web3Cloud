e00d47003ad834457f94c149e812ee9447fd622d
Pierre Untas <104831958+PierreAnders@users.noreply.github.com>
2024-05-27 09:34:57 +0000

    add documentHash proprerty in BlockchainFile model

--- src/app/models/BlockchainFile.ts
+++ src/app/models/BlockchainFile.ts
@@ -3,11 +3,13 @@ import { FileMetadata } from "./FileMetadata";
 export class BlockchainFile extends FileMetadata {
     transactionHash: string;
     certificationContract: string;
+    documentHash: string;
 
-    constructor(name: string, uri: string, category: string, isPrivate: boolean, owner: string, transactionHash: string, certificationContract: string) {
+    constructor(name: string, uri: string, category: string, isPrivate: boolean, owner: string, transactionHash: string, certificationContract: string, documentHash: string) {
         super(name, uri, category, isPrivate, owner);
         this.transactionHash = transactionHash;
         this.certificationContract = certificationContract;
+        this.documentHash = documentHash
     }
 
     timestampAndCertify(): void{
